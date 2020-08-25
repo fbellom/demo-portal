2636ec3ba58078db3f66d3a9fb84e884cbab707b
fbellom <fbello@hotmail.com>
2020-08-25 19:16:38 -0400

    More Testing to CICD

--- tests/portal-test.sh
+++ tests/portal-test.sh
@@ -2,12 +2,3 @@
 
 set -e # Halt on Error
 bundle exec htmlproofer nginx/www --disable-external
-
-#BUILD Docker Container
-docker-compose --file docker-cicd.yaml build 
-docker-compose --file docker-cicd.yaml up
-
-#Test Enpoint with CURL
-
-#TEAR DOWN Container
-docker-compose --file docker-cicd.yaml down
