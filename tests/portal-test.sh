8ae8be39626a917db38c6334573de6fee4af0d4c
fbellom <fbello@hotmail.com>
2020-08-25 18:16:25 -0400

    More Testing to CICD

--- tests/portal-test.sh
+++ tests/portal-test.sh
@@ -2,9 +2,3 @@
 
 set -e # Halt on Error
 bundle exec htmlproofer nginx/www --disable-external
-
-ruby -run -e httpd nginx/www -p 5000
-
-# Testing Enpoints
-STATUSCODE=$(curl -I http://localhost:5000/index.html &> /dev/stderr --write-out "%{http_code}") | if test $STATUSCODE 52; then exit 0; fi
-STATUSCODE=$(curl -I http://localhost:5000/portal.html &> /dev/stderr --write-out "%{http_code}") | if test $STATUSCODE 52; then exit 0; fi
