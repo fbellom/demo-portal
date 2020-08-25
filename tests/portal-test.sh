59ab0d0690389ef7b38e175a53895a8a8d5be5b4
fbellom <fbello@hotmail.com>
2020-08-25 19:02:34 -0400

    More Testing to CICD

--- tests/portal-test.sh
+++ tests/portal-test.sh
@@ -4,9 +4,12 @@ set -e # Halt on Error
 bundle exec htmlproofer nginx/www --disable-external
 
 #BUILD Docker Container
-docker-compose --file docker-cicd --build 
-docker-compose --file docker-cicd up -d
+docker-compose --file docker-cicd.yaml --build 
+docker-compose --file docker-cicd.yaml up
 
 #Test Enpoint with CURL
 STATUSCODE=$(curl -I http://localhost/index.html &> /dev/stderr --write-out "%{h    ttp_code}") | if test $STATUSCODE 52; then exit 0; fi
 STATUSCODE=$(curl -I http://localhost/portal.html &> /dev/stderr --write-out "%{    http_code}") | if test $STATUSCODE 52; then exit 0; fi
+
+#TEAR DOWN Container
+docker-compose --file docker-cicd.yaml down
