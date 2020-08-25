aa73b3c4df22d924316b23e3fa7636b964390c91
fbellom <fbello@hotmail.com>
2020-08-25 19:10:05 -0400

    More Testing to CICD

--- tests/portal-test.sh
+++ tests/portal-test.sh
@@ -8,8 +8,6 @@ docker-compose --file docker-cicd.yaml build
 docker-compose --file docker-cicd.yaml up
 
 #Test Enpoint with CURL
-STATUSCODE=$(curl -I http://localhost/index.html &> /dev/stderr --write-out "%{http_code}") | if test $STATUSCODE 52; then exit 0; fi
-STATUSCODE=$(curl -I http://localhost/portal.html &> /dev/stderr --write-out "%{http_code}") | if test $STATUSCODE 52; then exit 0; fi
 
 #TEAR DOWN Container
 docker-compose --file docker-cicd.yaml down
